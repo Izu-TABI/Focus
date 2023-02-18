f2a3b3de0ee8c4bb5bb1b82c08ce699d10dcf5fc
Izu-TABI <masakisama14@gmail.com>
2023-02-18 21:57:16 +0900

    [Update] DigitalTimer.jsx

--- src/components/timer/DigitalTimer.jsx
+++ src/components/timer/DigitalTimer.jsx
@@ -1,39 +1,49 @@
 import React, {useState, useEffect} from 'react'
-import { add } from '../../database/add';
 
 let digitalId;
 let elapsedTime;
 
 const DigitalTimer = (props) => {
     const [time, setTime] = useState(0);
+    const [elapsedTimeState, setelapsedTimeState] = useState(0);
+    const [startTime, setStartTime] = useState(0);
 
+    //設定時間または開始、保存ボタンが押された場合
     useEffect(() => {
-        if (props.paused) setTime(props.seconds)
+        if (props.paused) {
+          setTime(props.seconds)
+          setStartTime(new Date())
+        }
         else {
             setTime(0)
         }
-        
-        console.log(props.paused)
     }, [props.seconds, props.paused])
 
-
+    // 設定時間が変更された場合
     useEffect(() => {
       digitalId = setInterval(() => {
-        if (props.paused === false || time === 0) {
-            elapsedTime = 0
-            clearInterval(digitalId)
-        } else if (time > 0) {
-            setTime(time - 1)
-            elapsedTime = props.seconds - time + 1
+        let now = 0;
+
+        //現在時刻
+        now = new Date()
+
+        //経過時間を算出
+        elapsedTime = startTime === 0 ? 0 : Math.floor((now - startTime) / 1000) 
+
+        setelapsedTimeState(elapsedTime)
+
+        if (props.paused === false || elapsedTime === props.seconds) {
+          setStartTime(0)
+          clearInterval(digitalId)
         }
       }, 1000);
       return () => clearInterval(digitalId);
     }, [time]);
 
     return (
-      <p id="digital-timer"><br/>残り: {Math.floor(time / 3600)}時間{Math.ceil(time % 3600 / 60)}分</p>
+      <p id="digital-timer"><br/>残り: {Math.floor(time / 3600)}時間{Math.ceil(time % 3600 / 60)}分<br />{elapsedTimeState}</p>
     );
 }
 
 export default DigitalTimer
-export {digitalId, elapsedTime}
+export {digitalId, elapsedTime} 
