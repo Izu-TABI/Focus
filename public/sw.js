44037c5a3fa0398476e62e7a0324d740c029dc52
Izu-TABI <masakisama14@gmail.com>
2023-02-23 21:12:58 +0900

    [Add] fullscreen for mobile

---
+++ public/sw.js
@@ -0,0 +1,20 @@
+// インストール
+self.addEventListener('install', (event) => {
+ 
+    // とりあえず何もしません
+ 
+});
+ 
+// 起動
+self.addEventListener('activate', (event) => {
+ 
+    // とりあえず何もしません
+ 
+});
+ 
+// ネットワークリクエスト
+self.addEventListener('fetch', (event) => {
+ 
+    // とりあえず何もしません
+ 
+});
