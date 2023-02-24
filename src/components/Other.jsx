2e1e548d7b445040a582342d5a4a8784b2f7de35
Izu-TABI <masakisama14@gmail.com>
2023-02-24 23:11:42 +0900

    [Add] text for discord

--- src/components/Other.jsx
+++ src/components/Other.jsx
@@ -8,6 +8,19 @@ const Other = () => {
             <span className="dicord-title">Discord</span>
             <a href="https://discord.gg/uwtS6Ucf" rel="noreferrer noopener" target="_blank"><img src={DiscordIcon} alt="" style={{width: '10vh', height: '10vh'}}/></a>
         </div>
+        <div className='text-center about-discord'>
+          <h6>Discordでできること</h6>
+          <ul>
+            <li>
+              皆の頑張りが見える
+            </li>
+            <li>
+              質問し合うことができる
+            </li>
+          </ul>
+          <br />
+          上のアイコンから参加してください
+        </div>
     </>
   )
 }
