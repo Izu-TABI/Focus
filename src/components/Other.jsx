1ab0069b5fb76e999dc0c778ee05a6088f6d6c9a
Izu-TABI <masakisama14@gmail.com>
2023-03-05 21:57:23 +0900

    [Update] text

--- src/components/Other.jsx
+++ src/components/Other.jsx
@@ -12,9 +12,8 @@ const Other = () => {
           </div>
           <div className='text-center about-discord'>
             <h6>Discordでできること</h6><br />
-            <p>皆の頑張りが見える</p>
+            <p>他の人の作業時間を見られる</p>
             <p>質問し合うことができる</p>
-    
             <br />
             <p> ぜひ上のアイコンから参加してみてください。</p>
           </div>
@@ -23,9 +22,9 @@ const Other = () => {
             <span className='recomend-title text-center'>推奨事項</span><br />
             ホーム画面へのショートカットに対応しています。ぜひご利用ください。<br /><br /><br />
             <span className='sub-title'>iPhone</span><br />
-            ①&nbsp;Safariの下部にある共有アイコンをタップ → ②「ホーム画面に追加」をタップ → ③&nbsp;右上の「追加」をタップ<br /><br /><br />
+            1.&nbsp;Safariの下部にある共有アイコンをタップ → 2.「ホーム画面に追加」をタップ → 3.&nbsp;右上の「追加」をタップ<br /><br /><br />
             <span className='sub-title'>Android</span> <br />
-            ①&nbsp;ブックマークに追加 → ②&nbsp;ブックマークしたページを長押し → ③「ショートカットを作成」をタップ
+            1.&nbsp;ブックマークに追加 → 2.&nbsp;ブックマークしたページを長押し → 3.「ショートカットを作成」をタップ
           </div>
 
           <div className="feedback-area text-center">
