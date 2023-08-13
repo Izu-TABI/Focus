12a12ea387bf1e055fa256b705c42ecd6f68fd57
Izu-TABI <masakisama14@gmail.com>
2023-08-13 14:47:17 +0900

    Fix: an error of update graph

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,39 +1,41 @@
 import React, { useEffect } from 'react'
 import Timer from './timer/Timer'
-import { auth } from '../database/firebase' 
+import { auth } from '../database/firebase'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import Startpage from './StartPage'
+import { updateTodayTotal } from '../database/updateTodayTotal'
 
 const Home = () => {
     const [user] = useAuthState(auth)
+    updateTodayTotal()
     useEffect(() => {
-        if (!user){
+        if (!user) {
             document.querySelector('.bottom-nav').style.display = 'none'
             document.querySelector('.side-nav').style.display = 'none'
         } else {
             document.querySelector('.bottom-nav').style.display = 'block'
             document.querySelector('.side-nav').style.display = 'block'
         }
-    }, [user]) 
+    }, [user])
 
-  return (
-    <>
-        <div className="main-contents-area">
-        {
-            user ? (
-                <> 
-                    <Timer></Timer>
-                </>
-                ) : (
-                    <>
-                        <Startpage></Startpage>
-                    </>
-            )
-        }
-        </div>
-        
-    </>
-  )
+    return (
+        <>
+            <div className="main-contents-area">
+                {
+                    user ? (
+                        <>
+                            <Timer></Timer>
+                        </>
+                    ) : (
+                        <>
+                            <Startpage></Startpage>
+                        </>
+                    )
+                }
+            </div>
+
+        </>
+    )
 }
 
 export default Home
--- src/database/updateTodayTotal.js
+++ src/database/updateTodayTotal.js
@@ -2,18 +2,23 @@ import { doc, updateDoc } from 'firebase/firestore';
 import { db, auth } from './firebase';
 import { getDatabaseInfo } from './getDatabaseInfo';
 
+// ミリ秒を日に変換する(ms → days)
+const convertMillisecToDays = (millisec) => {
+    return parseInt(millisec / 1000 / 60 / 60 / 24);
+}
 
 
 // 読み込み時にタイムスタンプと違ったら今日の合計時間を0にする
 export async function updateTodayTotal() {
 
-     //usersのdocumentを取得
+    //usersのdocumentを取得
     const userRef = doc(db, 'users', auth.currentUser.uid);
 
     // 日付を取得
     let date = new Date();
+    console.log(date.getMonth())
     let todayString = date.getFullYear() + "/" + date.getMonth() + "/" + date.getDate();
-    let weekArray = [];
+    let weekArray = new Array(7).fill(0);
     let today = new Date().getDay()
     if (today === 0) {
         today = 6;
@@ -22,40 +27,30 @@ export async function updateTodayTotal() {
     }
 
 
-    getDatabaseInfo().then(async(database) => {
-    // データベースのタイムスタンプと今日の日付を比較する
+    getDatabaseInfo().then(async (database) => {
+        // データベースのタイムスタンプと今日の日付を比較する
 
         if (database.timestamp !== todayString) {
             const oldDateArray = database.timestamp.split('/')
             const oldDate = new Date(oldDateArray[0], oldDateArray[1], oldDateArray[2]);
             const diffMilliSec = date - oldDate;
-            const diffDays = parseInt(diffMilliSec / 1000 / 60 / 60 / 24);
+            const diffDays = convertMillisecToDays(diffMilliSec);
 
-            // console.log(diffDays);
-
-            if (diffDays > 6) {
-                for (let i = 0; i < 7; i++) {
-                    if (weekArray[i] !== 0) {
-                        weekArray[i] = 0;
-                    }
-                }    
-            } else {
+            if (diffDays < 6) {
                 weekArray = await database.aWeekTotalTime;
                 for (let i = today; i < 7; i++) {
-                    if (weekArray[i] !== 0) {
-                        weekArray[i] = 0;
-                    }
+                    weekArray[i] = 0;
                 }
             }
-            
+
             const data = {
                 todayTotal: 0,
                 timestamp: todayString,
                 aWeekTotalTime: weekArray
             }
-            await updateDoc(userRef, data)
+            await updateDoc(userRef, data);
         }
     })
 
-    
+
 }
